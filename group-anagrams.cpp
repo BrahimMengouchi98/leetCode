33ed5ef4684602bfe4c3cac609b6715767a4f2e7
BrahimMengouchi98 <brahim.mengouchi.20@gmail.com>
2025-02-02 22:32:50 +0100

    done

--- group-anagrams.cpp
+++ group-anagrams.cpp
@@ -1,1 +1,27 @@
-https://leetcode.com/problems/group-anagrams/
+class Solution {
+public:
+
+    vector<vector<string>> groupAnagrams(vector<string>& strs) 
+    {
+        vector<vector<string>> vec;
+        vector<string> v;
+        unordered_map<string, vector<string>> mp;
+        string ss;
+        for (int i = 0; i < strs.size(); i++)
+        {
+            ss = strs[i];
+            sort(ss.begin(), ss.end());
+            mp[ss].push_back(strs[i]);
+        }
+        
+        for (auto pair : mp) 
+        {
+            for (auto word : pair.second) {
+                v.push_back(word);
+            }
+            vec.push_back(v);
+            v.clear();
+        }
+        return vec;
+    }
+};
