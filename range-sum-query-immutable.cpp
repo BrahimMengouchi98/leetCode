c981bdda1db32befb866b364db017f990bb4a9bc
BrahimMengouchi98 <brahim.mengouchi.20@gmail.com>
2025-05-29 13:41:52 +0100

    done

--- range-sum-query-immutable.cpp
+++ range-sum-query-immutable.cpp
@@ -1,30 +1,22 @@
 class NumArray {
 public:
-    vector<int> _nums, _nums2;
-    NumArray(vector<int>& nums) : _nums(nums.size()) {
-
-        _nums[0] = nums[0];
-        _nums2 = nums;
-        for (int i = 1; i < nums.size(); i++)
+    vector<int> _nums;
+    vector<int> sums;
+    NumArray(vector<int>& nums) : _nums(nums) 
+    {
+        int size = _nums.size();
+        int sum = 0;
+        sums.resize(size);
+        for (int i = 0; i < size; i++)
         {
-            _nums[i] = _nums[i - 1] + nums[i];
+            sum += _nums[i];
+            sums[i] = sum;
         }
     }
-    
+
     int sumRange(int left, int right) {
-        if (left - 1 >= 0)
-            return  _nums[right] - _nums[left - 1];
-        int sum = 0;
-        for (int i = left; i <= right; i++)
-        {
-            sum += _nums2[i];
-        }
-        return sum;
+        if (left == 0)
+            return sums[right];
+        return sums[right] - sums[left - 1];
     }
 };
-
-/**
- * Your NumArray object will be instantiated and called as such:
- * NumArray* obj = new NumArray(nums);
- * int param_1 = obj->sumRange(left,right);
- */
